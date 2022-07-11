77902a0d89a18d03584c17843a9a2087ca7a50e1
juan david marin <juaanmarin2007@gmail.com>
2022-07-11 15:17:56 -0500

    agrego el proyecto con el esquema de carpetas para la api

---
+++ src/main/java/com/purple_shop/ApiPurpleShopApplication.java
@@ -0,0 +1,13 @@
+package com.purple_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPurpleShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPurpleShopApplication.class, args);
+	}
+
+}
